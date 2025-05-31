f4ac775bea7922efbe48d1be691350b445104ae1
mgcvale <miguelcvalealt@gmail.com>
2025-05-31 00:02:15 -0300

    initial project commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let count: number = $state(0);
+  const increment: () => void = () => count++;
+</script>
+
+<main>
+  <h1>Typescript and <span class="text-sky-600">TailwindCSS</span> test</h1>
+  <button onclick={increment}>Count is {count}</button>
+</main>
